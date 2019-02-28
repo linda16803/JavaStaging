363437097dfec89bd8bbaab349cc935a7faf9f7b
lihong <lihong@DESKTOP-KLLFUES>
2019-02-28 15:38:35 -0500

    [999999] test my input.

---
+++ JavaDaily/query.java
@@ -0,0 +1,16 @@
+
+public class dictinary_check() {
+	public string [] strarr;
+	
+	public string [] sortedArry;
+	
+	dictionary_check(s){
+		this.strarr = s
+				
+	}
+	
+	sorted_arr() {
+		
+	}
+
+}
